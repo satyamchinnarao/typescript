86bc697500fb18367cdbb972a5080182cf42bfb8
Satyam Rao <satyam.rao@voicefoundry.com>
2024-04-08 21:42:25 +0530

    UTSD-191 math funcitons

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,3 +2,7 @@
 
 - 8/4/2024 - UTSD-192
   - Hello World Function
+
+
+- 8/4/2024 - UTSD-191
+  - Math Functions
---
+++ Maths/index.ts
@@ -0,0 +1,62 @@
+const firstNumber: number = 0;
+
+const secondNumber: number = 0;
+// addition of 2 numbers
+operation('plus', 100, 200);
+
+
+// Multiplication of 2 numbers
+operation('multiply', 100, 200);
+
+// Division of 2 numbers
+operation('divide', 100, 200);
+
+
+// Division Round Down of 2 numbers
+operation('dividerounddown', 100, 200);
+
+
+// Division round up of 2 numbers
+operation('divideroundup', 100, 200);
+
+
+// Biggest Number of 2 numbers
+operation('biggest', 100, 200);
+
+
+// Biggest Number of 2 numbers
+operation('biggest', 0, 0);
+
+
+// invalid Operation
+operation('biggestss', 10, 220);
+
+ function operation(flag: string, firstNumber: number, secondNumber: number) {
+    if (firstNumber > 0 && secondNumber > 0) {
+        switch (flag) {
+            case 'plus':
+                console.log((firstNumber + secondNumber), 'Addition of 2 Numbers');
+                break;
+            case 'multiply':
+                console.log((firstNumber * secondNumber), 'Multiplication of 2 Numbers');
+                break;
+            case 'divide':
+                console.log((firstNumber / secondNumber), 'Division of 2 Numbers');
+                break;
+            case 'dividerounddown':
+                console.log(Math.round(firstNumber / secondNumber), 'Division round Down of 2 Numbers');
+                break;
+            case 'divideroundup':
+                console.log(Math.floor(firstNumber / secondNumber), 'Division round UP of 2 Numbers');
+                break;
+            case 'biggest':
+                console.log(Math.max(firstNumber, secondNumber), 'Biggest no from the 2 Numbers');
+                break;
+            default:
+                console.log('Please provide a Valid Operation to perform');
+        }
+    } else {
+        console.log('Both Numbers cant be Zero');
+    }
+
+}
